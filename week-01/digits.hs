4728fffdee34eaa3a71bec229ee548d4857f8028
awangelo <anggg.gaio@gmail.com>
2025-06-27 16:29:49 -0300

    exercicios 1 e 2 do Credit Card

--- week-01/digits.hs
+++ week-01/digits.hs
@@ -1,1 +1,19 @@
 {-# OPTIONS_GHC -Wall #-}
+
+-- sumDigits :: [Integer] -> [Integer]
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther xs = reverse (doubleOther (reverse xs))
+
+doubleOther :: [Integer] -> [Integer]
+doubleOther (x : y : zs) = x : (y * 2) : doubleOther zs
+doubleOther [x]          = [x]
+doubleOther _            = []
+
+toDigits :: Integer -> [Integer]
+toDigits n = reverse (toDigitsRev n)
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev n
+    | n > 0     = (n `mod` 10) : toDigitsRev (n `div` 10)
+    | otherwise = []

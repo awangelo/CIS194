d49ace97e1b339d3dda15691b984ab7c6b39e089
awangelo <anggg.gaio@gmail.com>
2025-07-02 20:22:15 -0300

    parse de logs

--- week-02/LogAnalysis.hs
+++ week-02/LogAnalysis.hs
@@ -1,1 +1,22 @@
 {-# OPTIONS_GHC -Wall #-}
+
+module LogAnalysis where
+
+import Log
+
+parseMessage :: String -> LogMessage
+parseMessage logLine =
+    case words logLine of
+    ("E" : sev : ts : rest) -> LogMessage (Error $ read sev) (read ts) $ unwords rest
+    ("I" : ts : rest)       -> LogMessage Info (read ts) $ unwords rest
+    ("W" : ts : rest)       -> LogMessage Warning (read ts) $ unwords rest
+    _                       -> Unknown logLine
+
+
+parse :: String -> [LogMessage]
+parse = map parseMessage . lines
+
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) mt = mt
+insert msg Leaf = Node Leaf msg Leaf

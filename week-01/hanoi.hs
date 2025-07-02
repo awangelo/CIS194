afd73b13bdb09ffab7be18a4da310821e8730a05
awangelo <anggg.gaio@gmail.com>
2025-07-01 23:19:56 -0300

    hanoi com 4 pinos (haha)

--- week-01/hanoi.hs
+++ week-01/hanoi.hs
@@ -1,4 +1,5 @@
 {-# OPTIONS_GHC -Wall #-}
+{-# OPTIONS_GHC -Wno-type-defaults #-}
 
 type Peg = String
 type Move = (Peg, Peg)
@@ -12,5 +13,11 @@ hanoi x s e t = hanoi (x-1) s t e ++ [(s, e)] ++ hanoi (x-1) t e s
 -- hanoi 3 "a" "b" "c" == [("a", "b"), ("a", "c"), ("b", "c"), ("a", "b"), ("c", "a"), ("c", "b"), ("a", "b")
 --                                                           maior no lugar
 
-hanoi4 :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi4 = undefined
+-- https://en.wikipedia.org/wiki/Tower_of_Hanoi#Frame%E2%80%93Stewart_algorithm
+hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
+hanoi4 0 _ _ _ _ = []
+hanoi4 x s e t1 t2 =
+  let k = x - round (sqrt (2 * fromIntegral x + 1)) + 1
+  in  hanoi4 k s t1 t2 e -- move k discos do topo para uma vareta auxiliar
+      ++ hanoi (x - k) s e t2 -- move os discos de baixo (x - k) para o final, sem atrapalhar a vareta com os k discos (usando apenas 3 varetas)
+      ++ hanoi4 k t1 e s t2 -- termina movendo os k discos do topo para o final

6352238f9e036b3e2b5ad750b4d6fc283feb951f
awangelo <anggg.gaio@gmail.com>
2025-07-03 13:58:06 -0300

    LogAnalysis completo, e o culpado eh Alice

--- week-02/LogAnalysis.hs
+++ week-02/LogAnalysis.hs
@@ -1,4 +1,6 @@
 {-# OPTIONS_GHC -Wall #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use foldr" #-}
 
 module LogAnalysis where
 
@@ -18,5 +20,34 @@ parse = map parseMessage . lines
 
 
 insert :: LogMessage -> MessageTree -> MessageTree
-insert (Unknown _) mt = mt
-insert msg Leaf = Node Leaf msg Leaf
+insert (Unknown _) tree              = tree
+insert msg         Leaf              = Node Leaf msg Leaf
+insert _ tree@(Node _ (Unknown _) _) = tree
+insert msg@(LogMessage _ time1 _) (Node left msg2@(LogMessage _ time2 _) right)
+    | time1 > time2                  = Node left msg2 (insert msg right)
+    | otherwise                      = Node (insert msg left) msg2 right
+
+
+build :: [LogMessage] -> MessageTree
+build []         = Leaf
+build (m : msgs) = insert m (build msgs)
+
+-- ou `build = foldr insert Leaf`
+--
+-- foldr insert Leaf [m1, m2, m3]
+-- = insert m1 (insert m2 (insert m3 Leaf))
+--
+--
+-- Ou, expandindo:
+--
+-- Node (Node (Node Leaf m3 Leaf) m2 Leaf) m1 Leaf
+
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf                  = []
+inOrder (Node left msg right) = inOrder left ++ [msg] ++ inOrder right
+
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong msgs =
+  [msg | LogMessage (Error sev) _ msg <- inOrder (build msgs), sev >= 50]
